3388f76295ed912a0e3b2dca88e9d3b011467499
krbex <aidan.krbec5@gmail.com>
2023-01-18 21:07:06 -0600

    reducers added

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,10 +1,50 @@
 import { useReducer } from "react";
 import {
-
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 export const reducer = (state, action) => {
   switch (action.type) {
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
 
     default:
       return state;
@@ -12,5 +52,5 @@ export const reducer = (state, action) => {
 };
 
 export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState)
+  return useReducer(reducer, initialState);
 }
