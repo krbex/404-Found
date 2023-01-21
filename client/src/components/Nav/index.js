cac37a6dca6abea77dda76be438d79691cdba83d
JustinSnyder611 <JustinSnyder611@gmail.com>
2023-01-20 18:35:14 -0600

    added page styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,22 +2,43 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="">
-      <p className="">
-        <a href="/profile">Profile</a>
-      </p>
-      <p className="nav-item">
-        <a href="/login">Login</a>
-      </p>
-      <p className="nav-item">
-        <a href="/game">Games</a>
-      </p>
-      <p className="nav-item">
-        <a href="/home">Home</a>
-      </p>
-      <p className="nav-item">
-        <a href="/signup">Sign Up</a>
-      </p>
+    // <div className="">
+    //   <p className="">
+    //     <a href="/profile">Profile</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/login">Login</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/game">Games</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/home">Home</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/signup">Sign Up</a>
+    //   </p>
+    // </div>
+
+
+
+    <div>
+      <nav class="navigationWrapper">
+        <div class="logoWrapper">
+          <span class="stylish">Gaming</span>
+          <span class="logo">Gods</span>
+        </div>
+        <ul class="navigation">
+          <li class="parent"><a class="link" href="/">Home</a></li>
+
+          <li class="parent" id="clients">
+            <a class="link" href="/game"><i class="fas fa-minus"></i> Games <i class="fas fa-plus"></i></a>
+          </li>
+          <li class="parent"><a class="link" href="/profile">Profile</a></li>
+          <li class="parent"><a class="link" href="/login">Login</a></li>
+          <li class="parent"><a class="link" href="/signup">Sign Up</a></li>
+        </ul>
+      </nav>
     </div>
   );
 };
