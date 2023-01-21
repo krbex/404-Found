9647bb593c04b434c29ed15a0a69c561ce1b2f53
okingdomz <rkbarnes0310@gmail.com>
2023-01-20 22:19:46 -0600

    hi

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,62 +1,45 @@
 
-// autherization link to connect to log in and log out feature
-// import Auth from "../../utils/auth";
-//  using to link to login and log out function with autherization
-import { Link } from "react-router-dom";
-
-import "./Nav.css";
-
-import React, { useState } from "react";
-
-
-import { FaBars, FaTimes } from "react-icons/fa";
+import React from "react";
 
 const Nav = () => {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-
-  const [color, setColor] = useState(false);
-  const changeColor = () => {
-    if (window.scrollY >= 100) {
-      setColor(true);
-    } else {
-      setColor(false);
-    }
-  };
-
-  window.addEventListener("scroll", changeColor);
-
-
-
   return (
-    <div className={color ? "header header-bg" : "header"}>
-      <Link to="/">
-      
-      </Link>
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/game">Games</Link>
-        </li>
-        <li>
-          <Link to="/login">Login In</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign up</Link>
-        </li>
-      </ul>
-      <div className="hamburger" onClick={handleClick}>
-        {click ? (
-          <FaTimes size={20} style={{ color: "#000000" }} />
-        ) : (
-          <FaBars size={20} style={{ color: "#000000" }} />
-        )}
-      </div>
+    // <div className="">
+    //   <p className="">
+    //     <a href="/profile">Profile</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/login">Login</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/game">Games</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/home">Home</a>
+    //   </p>
+    //   <p className="nav-item">
+    //     <a href="/signup">Sign Up</a>
+    //   </p>
+    // </div>
+
+
+
+    <div>
+      <nav class="navigationWrapper">
+        <div class="logoWrapper">
+          <span class="stylish">Gaming</span>
+          <span class="logo">Gods</span>
+        </div>
+        <ul class="navigation">
+          <li class="parent"><a class="link" href="/">Home</a></li>
+
+          <li class="parent" id="clients">
+            <a class="link" href="/game"><i class="fas fa-minus"></i> Games <i class="fas fa-plus"></i></a>
+          </li>
+          <li class="parent"><a class="link" href="/profile">Profile</a></li>
+          <li class="parent"><a class="link" href="/login">Login</a></li>
+          <li class="parent"><a class="link" href="/signup">Sign Up</a></li>
+        </ul>
+      </nav>
     </div>
   );
 };
