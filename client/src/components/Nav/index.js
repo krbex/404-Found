0376745c97212f390693a9d85dcbfe8e2f13873d
Isaac Falcon <ice6490@gmail.com>
2023-01-21 19:36:58 -0600

    added login logout button for navbar, non functioning yet

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,46 +1,66 @@
 import React from "react";
+import Auth from "../../utils/auth";
 
 const Nav = () => {
-  return (
-    // <div className="">
-    //   <p className="">
-    //     <a href="/profile">Profile</a>
-    //   </p>
-    //   <p className="nav-item">
-    //     <a href="/login">Login</a>
-    //   </p>
-    //   <p className="nav-item">
-    //     <a href="/game">Games</a>
-    //   </p>
-    //   <p className="nav-item">
-    //     <a href="/home">Home</a>
-    //   </p>
-    //   <p className="nav-item">
-    //     <a href="/signup">Sign Up</a>
-    //   </p>
-    // </div>
 
-
-
-    <div>
-      <nav class="navigationWrapper">
-        <div class="logoWrapper">
-          <span class="stylish">Gaming</span>
-          <span class="logo">Gods</span>
-        </div>
-        <ul class="navigation">
-          <li class="parent"><a class="link" href="/">Home</a></li>
-
-          <li class="parent" id="clients">
-            <a class="link" href="/game"><i class="fas fa-minus"></i> Games <i class="fas fa-plus"></i></a>
-          </li>
-          <li class="parent"><a class="link" href="/profile">Profile</a></li>
-          <li class="parent"><a class="link" href="/login">Login</a></li>
-          <li class="parent"><a class="link" href="/signup">Sign Up</a></li>
+  function showCurrent() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul>
+          <a href="/" onClick={() => Auth.logout()}>
+            Logout
+          </a>
         </ul>
-      </nav>
-    </div>
-  );
+      );
+    } else {
+      return (
+      <ul>
+        <li class="parent">
+          <a class="link" href="/login">
+            Login
+          </a>
+        </li>
+      </ul>
+      );
+    }
+  }
+
+    return (
+      <div>
+        <nav class="navigationWrapper">
+          <div class="logoWrapper">
+            <span class="stylish">Gaming</span>
+            <span class="logo">Gods</span>
+          </div>
+          <ul class="navigation">
+            <li class="parent">
+              <a class="link" href="/">
+                Home
+              </a>
+            </li>
+
+            <li class="parent" id="clients">
+              <a class="link" href="/game">
+                <i class="fas fa-minus"></i> Games <i class="fas fa-plus"></i>
+              </a>
+            </li>
+            <li class="parent">
+              <a class="link" href="/profile">
+                Profile
+              </a>
+            </li>
+            <li>
+              {showCurrent()}
+            </li>
+            <li class="parent">
+              <a class="link" href="/signup">
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
 };
 
 export default Nav;
