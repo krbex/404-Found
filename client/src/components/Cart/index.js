fff2f8bcf0fb7469b19892def15b2303d3db83a6
shelldan <shelldanlee@gmail.com>
2023-01-23 21:57:21 -0600

    With help

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,115 +1,124 @@
-import React, { useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { useLazyQuery } from "@apollo/client";
-import { QUERY_CHECKOUT } from "../../utils/queries";
-import { idbPromise } from "../../utils/helpers";
-import CartItem from "../CartItem";
-import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_TO_CART } from "../../utils/actions";
-import "./style.css";
+import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART, ADD_TO_CART } from '../../utils/actions';
+import './style.css';
 
 // stripePromise returns a promise with the stripe object as soon as the Stripe package loads
-const stripePromise = loadStripe('pk_test_51MTHHZH7A1ByAq6eouN4l6VYB9aCVQFqc2jJkppIYAn09SDn9JFl2v2Fq5kAQp7RQwH8XXezrrjUSNIfVYFhxyhW00h0hLL3kc');
+const stripePromise = loadStripe(
+	'pk_test_51MTHHZH7A1ByAq6eouN4l6VYB9aCVQFqc2jJkppIYAn09SDn9JFl2v2Fq5kAQp7RQwH8XXezrrjUSNIfVYFhxyhW00h0hLL3kc'
+);
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
-  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+	const [state, dispatch] = useStoreContext();
+	const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
-  // We check to see if there is a data object that exists, if so this means that a checkout session was returned from the backend
-  // Then we should redirect to the checkout with a reference to our session id
-  useEffect(() => {
-    if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
-    }
-  }, [data]);
+	// We check to see if there is a data object that exists, if so this means that a checkout session was returned from the backend
+	// Then we should redirect to the checkout with a reference to our session id
+	useEffect(() => {
+		if (data) {
+			stripePromise.then((res) => {
+				res.redirectToCheckout({ sessionId: data.checkout.session });
+			});
+		}
+	}, [data]);
 
-  // If the cart's length or if the dispatch function is updated, check to see if the cart is empty.
-  // If so, invoke the getCart method and populate the cart with the existing from the session
-  useEffect(() => {
-    async function getCart() {
-      const cart = await idbPromise("cart", "get");
-      dispatch({ type: ADD_TO_CART, games: [...cart] });
-    }
+	// If the cart's length or if the dispatch function is updated, check to see if the cart is empty.
+	// If so, invoke the getCart method and populate the cart with the existing from the session
 
-    if (!state.cart.length) {
-      getCart();
-    }
-  }, [state.cart.length, dispatch]);
+	useEffect(() => {
+		async function getCart() {
+			const cart = await idbPromise('cart', 'get');
+			console.log('here is the cart');
+			console.log(cart);
+			if (cart.length) {
+				dispatch({ type: ADD_TO_CART, games: [...cart] });
+			} else {
+				dispatch({ type: ADD_TO_CART, games: [] });
+			}
+		}
 
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
+		if (!state.cart.length) {
+			getCart();
+		}
+	}, []);
 
-  function calculateTotal() {
-    let sum = 0;
-    state.games.forEach((game) => {
-      sum += game.price * game.purchaseQuantity;
-    });
-    return sum.toFixed(2);
-  }
+	function toggleCart() {
+		dispatch({ type: TOGGLE_CART });
+	}
 
-  // When the submit checkout method is invoked, loop through each item in the cart
-  // Add each item id to the productIds array and then invoke the getCheckout query passing an object containing the id for all our products
-  function submitCheckout() {
-    const gameIds = [];
+	function calculateTotal() {
+		let sum = 0;
+		state.cart.forEach((game) => {
+			sum += game.price * game.purchaseQuantity;
+		});
+		return sum.toFixed(2);
+	}
 
-    state.cart.forEach((game) => {
-      for (let i = 0; i < game.purchaseQuantity; i++) {
-        gameIds.push(game._id);
-      }
-    });
+	// When the submit checkout method is invoked, loop through each item in the cart
+	// Add each item id to the productIds array and then invoke the getCheckout query passing an object containing the id for all our products
+	function submitCheckout() {
+		const gameIds = [];
 
-    getCheckout({
-      variables: { games: gameIds },
-    });
-  }
+		state.cart.forEach((game) => {
+			for (let i = 0; i < game.purchaseQuantity; i++) {
+				gameIds.push(game._id);
+			}
+		});
 
-  if (!state.cartOpen) {
-    return (
-      <div className="cart-closed" onClick={toggleCart}>
-        <span role="img" aria-label="trash">
-          🛒
-        </span>
-      </div>
-    );
-  }
+		getCheckout({
+			variables: { games: gameIds },
+		});
+	}
 
-  return (
-    <div className="cart">
-      <div className="close" onClick={toggleCart}>
-        [close]
-      </div>
-      <h2>Shopping Cart</h2>
-      {state.cart.length ? (
-        <div>
-          {state.games.map((game) => (
-            // console.log(game)
-            <CartItem key={game._id} game={game} />
-          ))}
+	if (!state.cartOpen) {
+		return (
+			<div className='cart-closed' onClick={toggleCart}>
+				<span role='img' aria-label='trash'>
+					🛒
+				</span>
+			</div>
+		);
+	}
 
-          <div className="flex-row space-between">
-            <strong>Total: ${calculateTotal()}</strong>
+	return (
+		<div className='cart'>
+			<div className='close' onClick={toggleCart}>
+				[close]
+			</div>
+			<h2>Shopping Cart</h2>
+			{state.cart.length ? (
+				<div>
+					{state.cart.map((game) => (
+						// console.log(game)
+						<CartItem key={game._id} game={game} />
+					))}
 
-            {Auth.loggedIn() ? (
-              <button onClick={submitCheckout}>Checkout</button>
-            ) : (
-              <span>(log in to check out)</span>
-            )}
-          </div>
-        </div>
-      ) : (
-        <h3>
-          <span role="img" aria-label="shocked">
-            😱
-          </span>
-          You haven't added anything to your cart yet!
-        </h3>
-      )}
-    </div>
-  );
+					<div className='flex-row space-between'>
+						<strong>Total: ${calculateTotal()}</strong>
+
+						{Auth.loggedIn() ? (
+							<button onClick={submitCheckout}>Checkout</button>
+						) : (
+							<span>(log in to check out)</span>
+						)}
+					</div>
+				</div>
+			) : (
+				<h3>
+					<span role='img' aria-label='shocked'>
+						😱
+					</span>
+					You haven't added anything to your cart yet!
+				</h3>
+			)}
+		</div>
+	);
 };
 
 export default Cart;
--- client/src/pages/GameDetail.js
+++ client/src/pages/GameDetail.js
@@ -1,103 +1,106 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 
-import Cart from "../components/Cart/index";
-import { useStoreContext } from "../utils/GlobalState";
-import { REMOVE_FROM_CART, ADD_TO_CART, UPDATE_GAMES } from "../utils/actions";
-import { QUERY_GAMES } from "../utils/queries";
-import { idbPromise } from "../utils/helpers";
-import spinner from "../assets/spinner.gif";
+import Cart from '../components/Cart/index';
+import { useStoreContext } from '../utils/GlobalState';
+import { REMOVE_FROM_CART, ADD_TO_CART, UPDATE_GAMES } from '../utils/actions';
+import { QUERY_GAMES } from '../utils/queries';
+import { idbPromise } from '../utils/helpers';
+import spinner from '../assets/spinner.gif';
 
 function GameDetail() {
-  const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
+	const [state, dispatch] = useStoreContext();
+	const { id } = useParams();
 
-  const [currentGame, setCurrentGame] = useState({});
+	const [currentGame, setCurrentGame] = useState({});
 
-  const { loading, data } = useQuery(QUERY_GAMES);
+	const { loading, data } = useQuery(QUERY_GAMES);
 
-  const { games, cart } = state;
-  console.log(state);
+	const { games, cart } = state;
+	console.log(state);
 
-  useEffect(() => {
-    // already in global store
-    if (games.length) {
-      setCurrentGame(data.games.find((game) => game._id === id));
-    }
-    // retrieved from server
-    else if (data) {
-      dispatch({
-        type: UPDATE_GAMES,
-        games: data.games,
-      });
+	useEffect(() => {
+		// already in global store
+		if (games.length) {
+			setCurrentGame(data.games.find((game) => game._id === id));
+		}
+		// retrieved from server
+		else if (data) {
+			dispatch({
+				type: UPDATE_GAMES,
+				games: data.games,
+			});
 
-      data.games.forEach((game) => {
-        idbPromise("games", "put", game);
-      });
-    }
-    // get cache from idb
-    else if (!loading) {
-      idbPromise("games", "get").then((indexedGames) => {
-        dispatch({
-          type: UPDATE_GAMES,
-          products: indexedGames,
-        });
-      });
-    }
-  }, [games, data, loading, dispatch, id]);
+			data.games.forEach((game) => {
+				idbPromise('games', 'put', game);
+			});
+		}
+		// get cache from idb
+		else if (!loading) {
+			idbPromise('games', 'get').then((indexedGames) => {
+				dispatch({
+					type: UPDATE_GAMES,
+					products: indexedGames,
+				});
+			});
+		}
+	}, [games, data, loading, dispatch, id]);
 
-  // const addToCart = () => {
-  //   const itemInCart = cart.find((cartItem) => cartItem._id === id);
-  //   if (itemInCart) {
-  //     dispatch({
-  //       type: ADD_TO_CART,
-  //       game: { ...currentGame, purchaseQuantity: 1 },
-  //     });
-  //     idbPromise("cart", "put", { ...currentGame, purchaseQuantity: 1 });
-  //   }
-  // };
+	const addToCart = () => {
+		// const itemInCart = cart.find((cartItem) => cartItem._id === id);
+		// if (itemInCart) {
+		console.log(currentGame);
+		dispatch({
+			type: ADD_TO_CART,
+			games: { ...currentGame, purchaseQuantity: 1 },
+		});
+		idbPromise('cart', 'put', { ...currentGame, purchaseQuantity: 1 });
+		// }
+	};
 
-  // const removeFromCart = () => {
-  //   dispatch({
-  //     type: REMOVE_FROM_CART,
-  //     _id: currentGame._id,
-  //   });
+	const removeFromCart = () => {
+		dispatch({
+			type: REMOVE_FROM_CART,
+			_id: currentGame._id,
+		});
 
-  //   idbPromise("cart", "delete", { ...currentGame });
-  // };
+		idbPromise('cart', 'delete', { ...currentGame });
+	};
 
-  return (
-    <>
-      {currentGame && cart ? (
-        <div className="currentGameBlock">
-          <Link to="/game">← Back to games</Link>
+	return (
+		<>
+			{currentGame && cart ? (
+				<div className='currentGameBlock'>
+					<Link to='/game'>← Back to games</Link>
 
-          <h2>{currentGame.name}</h2>
+					<h2>{currentGame.name}</h2>
 
-          <p>{currentGame.description}</p>
+					<p>{currentGame.description}</p>
+					<button onClick={() => addToCart()}>Add to Cart</button>
+					<button onClick={() => removeFromCart()}>Remove from Cart</button>
 
-          <img src={`/images/${currentGame.image}`} alt={currentGame.name} />
-          <p>Trailer:</p>
-          {/* <a href={currentGame.ytUrl}>
+					<img src={`/images/${currentGame.image}`} alt={currentGame.name} />
+					<p>Trailer:</p>
+					{/* <a href={currentGame.ytUrl}>
             <img
               src={`/images/${currentGame.youtube}`}
               alt={currentGame.name}
             />
           </a> */}
-          <iframe 
-          width="853"
-          height="480"
-          src={`https://www.youtube.com/embed/${currentGame.ytUrl}`}
-          title = "Embedded youtube"
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+					<iframe
+						width='853'
+						height='480'
+						src={`https://www.youtube.com/embed/${currentGame.ytUrl}`}
+						title='Embedded youtube'
+					/>
+				</div>
+			) : null}
+			{loading ? <img src={spinner} alt='loading' /> : null}
 
-      <Cart />
-    </>
-  );
+			<Cart />
+		</>
+	);
 }
 
 export default GameDetail;
